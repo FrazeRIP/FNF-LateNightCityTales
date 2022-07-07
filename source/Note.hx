efc7bec961a0793a6bbcea7b126381ef218224b3
LunarSurface <wonder_bernkastel@qq.com>
2022-07-07 16:40:08 +0800

    Note Fix

--- source/Note.hx
+++ source/Note.hx
@@ -317,7 +317,7 @@ class Note extends FlxSprite
 
 		if (isSustainNote)
 		{
-			animation.addByPrefix('purpleholdend', 'pruple end hold');
+			animation.addByPrefix('purpleholdend', 'purple hold end');
 			animation.addByPrefix('greenholdend', 'green hold end');
 			animation.addByPrefix('redholdend', 'red hold end');
 			animation.addByPrefix('blueholdend', 'blue hold end');
